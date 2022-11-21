9e53e6a6bd56f6ffd5ba1e75bcf61a8fd7c7df4e
nico <nico.rusig@gmail.com>
2022-11-21 23:04:06 +0100

    Update category articles admin message

--- components/layouts/categoryArticles.tsx
+++ components/layouts/categoryArticles.tsx
@@ -14,15 +14,26 @@ export const CategoryArticles: React.FC<CategoryArticlesProps> = ({ articles, is
   if (!articles || !Array.isArray(articles) || articles?.length == 0)
     return isAdmin && isCategoryPage ? (
       <div className='p-4 my-4 font-semibold text-center text-yellow-900 bg-yellow-100 border rounded-lg'>
-        If you want to add related articles : go in <strong>Pages</strong> (right side panel), select a <strong>Category name</strong>, and Save and Refresh to see updates.
+        If you want to add article related to this category : Tag the articles, go in <strong>Pages</strong> (right side
+        panel), select a <strong>Category name</strong>, and Save and Refresh to see updates.
       </div>
     ) : null;
 
   return (
-    <div className='container grid grid-cols-1 gap-6 px-2 mx-auto sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-      {articles?.map((article, idx: number) => {
-        return <CardWithImage key={idx} {...article} />;
-      })}
-    </div>
+    <>
+      {isAdmin && (
+        <div className='max-w-screen-xl px-4 py-8 mx-auto lg:py-16 lg:px-6'>
+          <div className='p-4 my-4 font-semibold text-center text-yellow-900 bg-yellow-100 border rounded-lg'>
+            If you want to add article related to this category : Tag the articles, go in <strong>Pages</strong> (right
+            side panel), select a <strong>Category name</strong>, and Save and Refresh to see updates.
+          </div>
+        </div>
+      )}
+      <div className='container grid grid-cols-1 gap-6 px-2 mx-auto sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+        {articles?.map((article, idx: number) => {
+          return <CardWithImage key={idx} {...article} />;
+        })}
+      </div>
+    </>
   );
 };
